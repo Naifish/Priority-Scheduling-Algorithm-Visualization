41864bfe6c909d9731981c5d1ea691830793ce08
Naifish Ali <nf533051@dal.ca>
2018-05-19 21:05:24 -0300

    Simulation Done

--- Priority-Scheduling-Algorithm-Visualization/Form1.Designer.cs
+++ Priority-Scheduling-Algorithm-Visualization/Form1.Designer.cs
@@ -106,6 +106,7 @@
             this.button4.TabIndex = 50;
             this.button4.Text = "Simulate";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -119,6 +120,7 @@
             this.button3.TabIndex = 49;
             this.button3.Text = "Sort through Priority ";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label14
             // 
--- Priority-Scheduling-Algorithm-Visualization/Form1.cs
+++ Priority-Scheduling-Algorithm-Visualization/Form1.cs
@@ -13,7 +13,6 @@ namespace Priority_Scheduling_Algorithm_Visualization
     public partial class Form1 : Form
     {
         public int Total_processes, Arival_time, Priority, Burst_time, process_number = 2, input_range, fromX = 150, fromY = 250, pointer = 0, new_lb = 0, lable_pointer = 0, store_total_TAT = 0, moveX = 10, moveY = 10, movebox = 0, from_top = 0, from_left = 0, move_me = 0, check = 0, show_time = 0;
-
         public int point1 = 0, point2 = 0, point3 = 0;
         string Process_name;
         string[] names;
@@ -360,5 +359,253 @@ namespace Priority_Scheduling_Algorithm_Visualization
                 MessageBox.Show("priority can be 1,2 or 3 please enter a valid priority");
             }
         }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //sorting all the arrays with the help of prio array sorting.
+
+            for (int i = 0; i < names.Length - 1; i++)
+            {
+                check++;
+                int temp_prio = prio[i];
+                int swaper = 0;
+
+                for (int j = i + 1; j < names.Length; j++)
+                {
+                    if (prio[j] < temp_prio)
+                    {
+                        temp_prio = prio[j];
+                        swaper = j;
+
+                        int save_p = prio[i];
+                        prio[i] = prio[swaper];
+                        prio[swaper] = save_p;
+
+                        string save_nm = names[i];
+                        names[i] = names[swaper];
+                        names[swaper] = save_nm;
+
+                        int save_arvT = arv_time[i];
+                        arv_time[i] = arv_time[swaper];
+                        arv_time[swaper] = save_arvT;
+
+                        int save_bT = bs_time[i];
+                        bs_time[i] = bs_time[swaper];
+                        bs_time[swaper] = save_bT;
+                    }
+                }
+            }
+
+            if (check == Total_processes - 1)
+            {
+                //sorting again for arrival time
+
+                //sorting all the arrays with the help of spicified priority number and arival_time array.
+
+                for (int i = 0; i < names.Length - 1; i++)
+                {
+                    int temp_arv = arv_time[i];
+                    int swaper = 0;
+
+                    for (int j = i + 1; j < names.Length; j++)
+                    {
+                        if (prio[j] == 1 && arv_time[j] < temp_arv)
+                        {
+                            temp_arv = arv_time[j];
+                            swaper = j;
+
+                            string save_nm = names[i];
+                            names[i] = names[swaper];
+                            names[swaper] = save_nm;
+
+                            int save_arvT = arv_time[i];
+                            arv_time[i] = arv_time[swaper];
+                            arv_time[swaper] = save_arvT;
+
+                            int save_bT = bs_time[i];
+                            bs_time[i] = bs_time[swaper];
+                            bs_time[swaper] = save_bT;
+                        }
+                    }
+                }
+
+                //sorting all the arrays with the help of spicified priority number and arival_time array.
+
+                for (int i = 0; i < names.Length - 1; i++)
+                {
+                    int temp_arv = arv_time[i];
+                    int swaper = 0;
+
+                    for (int j = i + 1; j < names.Length; j++)
+                    {
+                        if (prio[j] == 2 && arv_time[j] < temp_arv)
+                        {
+                            temp_arv = arv_time[j];
+                            swaper = j;
+
+                            string save_nm = names[i];
+                            names[i] = names[swaper];
+                            names[swaper] = save_nm;
+
+                            int save_arvT = arv_time[i];
+                            arv_time[i] = arv_time[swaper];
+                            arv_time[swaper] = save_arvT;
+
+                            int save_bT = bs_time[i];
+                            bs_time[i] = bs_time[swaper];
+                            bs_time[swaper] = save_bT;
+                        }
+                    }
+                }
+
+                //sorting all the arrays with the help of spicified priority number and arival_time array.
+
+                for (int i = 0; i < names.Length - 1; i++)
+                {
+                    int temp_arv = arv_time[i];
+                    int swaper = 0;
+
+                    for (int j = i + 1; j < names.Length; j++)
+                    {
+                        if (prio[j] == 3 && arv_time[j] < temp_arv)
+                        {
+                            temp_arv = arv_time[j];
+                            swaper = j;
+
+                            string save_nm = names[i];
+                            names[i] = names[swaper];
+                            names[swaper] = save_nm;
+
+                            int save_arvT = arv_time[i];
+                            arv_time[i] = arv_time[swaper];
+                            arv_time[swaper] = save_arvT;
+
+                            int save_bT = bs_time[i];
+                            bs_time[i] = bs_time[swaper];
+                            bs_time[swaper] = save_bT;
+                        }
+                    }
+                }
+            }
+
+
+
+
+            //changing colors of labels(processes) according to their priority which is sort above.
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (prio[i] == 1)
+                {
+                    myLable[i].BackColor = Color.Red;
+                }
+                else if (prio[i] == 2)
+                {
+                    myLable[i].BackColor = Color.Green;
+                }
+                else if (prio[i] == 3)
+                {
+                    myLable[i].BackColor = Color.Blue;
+                }
+            }
+
+
+            //copy the same code as above just to update table values after sorting.
+
+            //disposing privious datagridview
+            dataGridView1.Dispose();
+
+            //creating another gridview on same place
+            dataGridView2 = new DataGridView();
+            dataGridView2.Location = new Point(400, 400);
+            dataGridView2.Size = new Size(660, 40 * input_range);
+            dataGridView2.ColumnCount = 6;
+            dataGridView2.Columns[0].Name = "Process Name";
+            dataGridView2.Columns[1].Name = "Priority";
+            dataGridView2.Columns[2].Name = "Arrival Time";
+            dataGridView2.Columns[3].Name = "Burst Time";
+            dataGridView2.Columns[4].Name = "Responce Time";
+            dataGridView2.Columns[5].Name = "Total Turnarround Time";
+
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                row = new string[] { names[i], prio[i].ToString(), arv_time[i].ToString(), bs_time[i].ToString() };
+                dataGridView2.Rows.Add(row);
+            }
+            this.Controls.Add(dataGridView2);
+
+
+            //changing the names of labels(process) with the help of array indexis
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                myLable[i].Text = "    " + names[i] + " ser = " + bs_time[i];
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //enabling analog clock
+            pictureBox2.Visible = true;
+
+            //queue lable unvisibling
+
+            label13.Visible = false;
+
+            //geting position of current process in variables
+
+            from_top = myLable[move_me].Top;
+            from_left = myLable[move_me].Left;
+
+            t1 = new Timer();
+            t1.Interval = 1;
+            t1.Enabled = true;
+            t1.Tick += new System.EventHandler(On_perTick);
+
+
+            t2 = new Timer(); //jaise hi processor mai process phnch jae to timer start ho jae and utna hi chale jitna process ka service time ho and then process dispose ho jae.then timer pause ho kar again new process ane par start ho jae ye mechanism tb tk chalta rahe jb tak process khtm na ho jae.
+            t2.Interval = 1000;
+            t2.Enabled = false;
+            t2.Tick += new System.EventHandler(On_perTick2);
+
+            //t3.Start();
+        }
+        private void On_perTick(object sender, EventArgs e)
+        {
+            //moving processes towards processor
+
+            if (from_top != 172)
+            {
+                myLable[move_me].Location = new Point(myLable[move_me].Left, from_top--);
+            }
+            else if (from_left != 401)
+            {
+                myLable[move_me].Location = new Point(from_left++, from_top);
+            }
+            else
+            {
+                if (move_me != names.Length - 1)
+                {
+                    move_me++;
+                    from_top = myLable[move_me].Top;
+                    from_left = myLable[move_me].Left;
+                    t2.Enabled = true;
+
+
+                }
+            }
+        }
+        private void On_perTick2(object sender, EventArgs e)
+        {
+            show_time++;
+            label15.Text = show_time.ToString();
+
+            if (from_left == 401 && from_top == 172)
+            {
+                t2.Stop();
+                pictureBox2.Visible = false;
+            }
+        }
+
+
     }
 }
